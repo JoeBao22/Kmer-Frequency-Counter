6bff5acc97c1e10829c4992689abb50ab96f6ea6
Xiaoyi Bao <baoxiaoyi1999@gmail.com>
2021-03-05 11:21:16 +0800

    calculate probability when doing subtraction * use counter instead of frequency * do differently according to whether we combine reverse_complement or not * rename gross_f with all_coiunter

--- utils_kfc/vector_calculator.py
+++ utils_kfc/vector_calculator.py
@@ -20,63 +20,55 @@ def subtraction_counter_task(counter, encoder):
         probability[encoder(k)] = v / num_of_items
     return probability
     
-def feature_vector(gross_f, kmer_statistics):
+def feature_vector(all_counter, kmer_statistics):
     """
     given the original copy number, generate a list of frequency or copy number. Use subtraction or not
-    :param gross_f: {key: value}
+    :param gross_f: a list of counters
     :return: [(key:str, encoded_key: int, value: number)]
     """
     if kmer_statistics.subtraction:
-        # for str s, we get substrings:
-        # s1 = s[:-1] (remove the last character)
-        # s2: s[1:] (remove the first character)
-        # s3: s[1:-1] (remove the first and the last character)
-        # and we've got the frequency dicts for pattern s, s1, s2, s3 in freq, freq1, freq2, freq3, respectively
-        # the frequency dicts are grouped according to their patterns (positions being chosen in each substring)
-        # the background for key s is defined as :
-        # bgd(s) = freq_1(s1) * freq_2(s2) / freq_3(s3)
-        # we define the frequency with respect to background as :
-        # del_bgd(s) = freq(s) / bgd(s) - 1
-        # we will record the frequency of s as long as: s[1: ] in mid_dict
-        manager = Manager()
-        probability_res = []
-        shared_f = [manager.dict(item) for item in gross_f]
-        bgd = {}
-        del_bgd = {}
+        """
+        for str s, we get substrings:
+        s1 = s[:-1] (remove the last character)
+        s2: s[1:] (remove the first character)
+        s3: s[1:-1] (remove the first and the last character)
+        and we've got the frequency dicts for pattern s, s1, s2, s3 in freq, freq1, freq2, freq3, respectively
+        the frequency dicts are grouped according to their patterns (positions being chosen in each substring)
+        the background for key s is defined as :
+        bgd(s) = freq_1(s1) * freq_2(s2) / freq_3(s3)
+        we define the frequency with respect to background as :
+        del_bgd(s) = freq(s) / bgd(s) - 1
+        as the total number of items in each frequency dict are always the same, 
+        del_bgd(s) = (counter(s) * counter3(s3)) / (counter1(s1) * counter2(s2)) - 1
+        we will record the frequency of s as long as: s[1: ] in mid_dict
+        """
         assert kmer_statistics.frequency
-        pool =  Pool(kmer_statistics.core) 
-        for index, freq_count in enumerate(shared_f):
-            probability_res.append(pool.apply_async(subtraction_counter_task, 
-                            args=(freq_count, 
-                            kmer_statistics.encoder)))
-        pool.close()
-        pool.join()
-        probability = [item.get() for item in probability_res]
-
-        for (k, v) in gross_f[3].items():
+        del_bgd = []
+        for (k, v) in all_counter[3].items():
             for pre_ch in ["A", "C", "G", "T"]:
                 for after_ch in ["A", "C", "G", "T"]:
-                    full_k = get_smaller(pre_ch + k + after_ch)
-                    k_sub1 = get_smaller(pre_ch + k)
-                    k_sub2 = get_smaller(k + after_ch)
-                    k_sub3 = k
-                    if gross_f[1][k_sub1] and gross_f[2][k_sub2]:
-                        encoded_0, encoded_1, encoded_2, encoded_3 = kmer_statistics.encoder(full_k), kmer_statistics.encoder(
-                            k_sub1), kmer_statistics.encoder(k_sub2), kmer_statistics.encoder(k_sub3)
-                        bgd = probability[1][encoded_1] * probability[2][encoded_2] / probability[3][
-                            encoded_3]
-                        del_bgd[full_k] = ((full_k, encoded_0, probability[0].get(encoded_0, 0) / bgd - 1))
-        del_bgd_list = sorted(list(del_bgd.values()))
-        return del_bgd_list
+                    if kmer_statistics.combine:
+                        full_k = get_smaller(pre_ch + k + after_ch)
+                    else:
+                        full_k = pre_ch + k + after_ch
+                    k_sub1 = get_smaller(full_k[:-1])
+                    k_sub2 = get_smaller(full_k[1:])
+                    if all_counter[1][k_sub1] and all_counter[2][k_sub2]:
+                        prob = (all_counter[0][full_k] * all_counter[3][k]) / \
+                        (all_counter[1][k_sub1] * all_counter[2][k_sub2]) - 1
+                        encoded = kmer_statistics.encoder(full_k)
+                        del_bgd.append((full_k, encoded, prob))
+        del_bgd.sort()
+        return del_bgd
 
     else:
-        num_of_items = item_in_dict(gross_f[0])  # rename
+        num_of_items = item_in_dict(all_counter[0])  # rename
         with Pool(kmer_statistics.core) as pool:
             if kmer_statistics.frequency:
                 partial_res = partial(encoded_list,  encoder=kmer_statistics.encoder)
-                iterator_p = pool.imap_unordered(partial_res, gross_f[0].items(), chunksize=5000)
+                iterator_p = pool.imap_unordered(partial_res, all_counter[0].items(), chunksize=5000)
             else:
                 partial_res = partial(encoded_list_freq, num_of_items=num_of_items, encoder=kmer_statistics.encoder)
-                iterator_p = pool.imap_unordered(partial_res, gross_f[0].items(), chunksize=5000)
+                iterator_p = pool.imap_unordered(partial_res, all_counter[0].items(), chunksize=5000)
             probability = sorted(list(iterator_p))
             return probability
